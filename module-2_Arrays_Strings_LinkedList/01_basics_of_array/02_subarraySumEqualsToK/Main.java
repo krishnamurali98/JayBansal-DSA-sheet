058606bc6607f4ba106e66edd8ba21491908b98c
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2025-01-19 09:43:46 +0530

    Two Sum

--- module-2_Arrays_Strings_LinkedList/01_basics_of_array/02_subarraySumEqualsToK/Main.java
+++ module-2_Arrays_Strings_LinkedList/01_basics_of_array/02_subarraySumEqualsToK/Main.java
@@ -1,4 +1,6 @@
-class Solution {
+import java.util.HashMap;
+
+class BruteForce {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
         int ans = 0;
@@ -9,6 +11,26 @@ class Solution {
                 if(sum == k)
                     ans++;
             }
+        }
+        return ans;
+    }
+}
+
+class PrefixSum {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, 1);
+        
+        int sum = 0;
+        int ans = 0;
+
+        for(int n : nums) {
+            sum += n;
+            if(hm.containsKey(sum-k)) {
+                ans += hm.get(sum-k);
+            } 
+
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
         }
         return ans;
     }
