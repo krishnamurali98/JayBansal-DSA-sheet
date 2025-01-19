072fc81495c7f29dd6febe96ed03e800daad8570
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2025-01-19 10:11:04 +0530

    Two Sum updated

--- module-2_Arrays_Strings_LinkedList/02_dutch_national_flag_moore_Kadane/01_TwoSum/Main.java
+++ module-2_Arrays_Strings_LinkedList/02_dutch_national_flag_moore_Kadane/01_TwoSum/Main.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
-class Solution {
+class Prefix {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> m = new HashMap<>();
         for(int i=0;i<nums.length;i++) {
@@ -10,5 +11,33 @@ class Solution {
             m.put(nums[i], i);
         }
         return new int[]{-1, -1};       
+    }
+}
+
+
+class SortAndTwoPointer {
+    public int[] twoSum(int[] nums, int k) {
+        
+        ArrayList<int[]> arr = new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++) {
+            arr.add(new int[]{nums[i], i});
+        }
+        
+        arr.sort((a, b) -> a[0]-b[0]);
+        
+        int i=0, j = nums.length-1;
+        while(i < j) {
+            int s = arr.get(i)[0] + arr.get(j)[0];
+            if(s == k) {
+                return new int[]{arr.get(i)[1], arr.get(j)[1]};
+            } else if(s > k) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return new int[]{-1, -1};      
     }
 }
