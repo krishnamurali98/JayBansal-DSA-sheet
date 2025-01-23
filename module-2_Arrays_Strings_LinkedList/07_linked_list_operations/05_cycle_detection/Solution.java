4dc83229eab50562dd19eb6db23349738664bf7a
krishnamurali98 <krishnamurali.pulabaigari@gmail.com>
2025-01-23 17:38:26 +0530

    Binary Search

--- module-2_Arrays_Strings_LinkedList/07_linked_list_operations/05_cycle_detection/Solution.java
+++ module-2_Arrays_Strings_LinkedList/07_linked_list_operations/05_cycle_detection/Solution.java
@@ -1,3 +1,13 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
+
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
